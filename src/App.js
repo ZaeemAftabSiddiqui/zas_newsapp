09e847f02f38b5ce9f941ace6f9a02762e41fcf7
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-06-07 13:14:36 +0500

    deleted some unwanted files and add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 import React, { Component } from "react";
 
 export default class App extends Component {
+  c = " zaeem";
   render() {
-    return <div>Hello my first class based component</div>;
+    return <div>Hello my first class based component{this.c}</div>;
   }
 }
