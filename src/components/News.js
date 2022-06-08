c0042642ff22111174d73db720bb2c9c7d8c8c2f
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-06-08 16:36:42 +0500

    add news itmes component file

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+import NewsItems from "./NewsItems";
 
 export class News extends Component {
   render() {
-    return <div>This is a news component</div>;
+    return (
+      <div>
+        This is a news component
+        <NewsItems />
+        <NewsItems />
+        <NewsItems />
+        <NewsItems />
+        <NewsItems />
+        <NewsItems />
+        <NewsItems />
+        <NewsItems />
+      </div>
+    );
   }
 }
 
---
+++ src/components/NewsItems.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class NewsItems extends Component {
+  render() {
+    return <div>This is a news items 1</div>;
+  }
+}
+
+export default NewsItems;
