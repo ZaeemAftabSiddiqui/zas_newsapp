ec6d84f0710ef78c30c406c994cb45cfe508c3ce
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-06-09 17:07:39 +0500

    loop for news

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 
 export class NewsItems extends Component {
   render() {
-    let { title, description, imageUrl } = this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
           <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/news" className="btn btn-sm btn-primary">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a
+              href={newsUrl}
+              target="-blank"
+              className="btn btn-sm btn-primary"
+            >
               Read News
             </a>
           </div>
