54937c6d4bc32d48aab05076d41f4d28d808315b
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-06-11 01:11:56 +0500

    add buttons and make 2 functions to used them

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -6,15 +6,19 @@ export class NewsItems extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              !imageUrl
+                ? "https://s2.glbimg.com/lU1QKIyaus4u4BxgHzIxxoSpe7g=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_63b422c2caee4269b8b34177e8876b93/internal_photos/bs/2021/K/K/nhFs4jQom3wZTA3GVK6g/watts-1-.jpeg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a
-              href={newsUrl}
-              target="-blank"
-              className="btn btn-sm btn-primary"
-            >
+            <a href={newsUrl} target="-blank" className="btn btn-sm btn-dark">
               Read News
             </a>
           </div>
