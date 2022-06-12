3d2a4e02b8703180779a604b2b2fc5d0b1076c4d
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-06-12 16:46:14 +0500

    add spinner and loading gif to show leading of web page news

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
     return (
       <div>
         <NavBar />
-        <News pageSize={20} />
+        <News pageSize={5} />
       </div>
     );
   }
--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItems from "./NewsItems";
+import { Spinner } from "./Spinner";
 
 export class News extends Component {
   constructor() {
@@ -12,11 +13,13 @@ export class News extends Component {
   }
   async componentDidMount() {
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8156b324bd974a02936a2357b2bfef10&page=1&pagesize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
+      loading: false,
     });
   }
 
@@ -24,29 +27,34 @@ export class News extends Component {
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8156b324bd974a02936a2357b2bfef10&page=${
       this.state.page - 1
     }&pagesize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       page: this.state.page - 1,
       articles: parsedData.articles,
+      loading: false,
     });
   };
 
   handleNextClick = async () => {
     console.log("Next");
     if (
-      this.state.page + 1 >
-      Math.ceil(this.state.totalResults / this.props.pageSize)
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalResults / this.props.pageSize)
+      )
     ) {
-    } else {
       let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8156b324bd974a02936a2357b2bfef10&page=${
         this.state.page + 1
       }&pagesize=${this.props.pageSize}`;
+      this.setState({ loading: true });
       let data = await fetch(url);
       let parsedData = await data.json();
       this.setState({
         page: this.state.page + 1,
         articles: parsedData.articles,
+        loading: false,
       });
     }
   };
@@ -54,21 +62,25 @@ export class News extends Component {
     return (
       <div className="container my-3">
         <h1 className="text-center">ZasGisNews - Top Headlines</h1>
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItems
-                  title={element.title ? element.title.slice(0, 45) : ""}
-                  description={
-                    element.description ? element.description.slice(0, 100) : ""
-                  }
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItems
+                    title={element.title ? element.title.slice(0, 45) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 100)
+                        : ""
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="conatainer d-flex justify-content-between">
           <button
