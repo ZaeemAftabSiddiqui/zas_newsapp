19b3dec50eafe1aff1c5f8ea983617a813be5974
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-06-22 18:43:14 +0500

    set alignment of news reference

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -7,6 +7,16 @@ export class NewsItems extends Component {
     return (
       <div className="my-3">
         <div className="card">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span className=" badge rounded-pill bg-danger ">{source}</span>
+          </div>
           <img
             src={
               !imageUrl
@@ -17,12 +27,6 @@ export class NewsItems extends Component {
             alt="..."
           />
           <div className="card-body">
-            <span
-              className="position-absolute top-0 translate-middle badge rounded-pill bg-danger "
-              style={{ left: "90%", zIndex: "1" }}
-            >
-              {source}
-            </span>
             <h5 className="card-title">{title} </h5>
             <p className="card-text">{description}...</p>
             <p className="card-text">
